6b0290ff7c373169d09f832f8d658f73ec38b6c1
Muzahid <muzahid.h@cisinlabs.com>
2022-01-11 17:46:32 +0530

    add sip py2

---
+++ sip.rb
@@ -0,0 +1,108 @@
+class Sip < Formula
+  desc "Tool to create Python bindings for C and C++ libraries"
+  homepage "https://www.riverbankcomputing.com/software/sip/intro"
+  url "https://dl.bintray.com/homebrew/mirror/sip-4.19.8.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/pyqt/sip/sip-4.19.8/sip-4.19.8.tar.gz"
+  sha256 "7eaf7a2ea7d4d38a56dd6d2506574464bddf7cf284c960801679942377c297bc"
+  revision 12
+  head "https://www.riverbankcomputing.com/hg/sip", :using => :hg
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6e71708515bb6b3b7956ac076058dbf4d2eeae470dace9563dbe9f313479a9b7" => :mojave
+    sha256 "c3b58c31be4c2014bb082731f25148ec65dce923d2499f72dc8eddcc95afa661" => :high_sierra
+    sha256 "d223031f11bfedd3247236109ce5bc0b7ff9a7e0672d44b4a4d1a129393d505c" => :sierra
+  end
+
+  depends_on "python"
+  depends_on "python@2"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
+    ENV.delete("SDKROOT") # Avoid picking up /Application/Xcode.app paths
+
+    if build.head?
+      # Link the Mercurial repository into the download directory so
+      # build.py can use it to figure out a version number.
+      ln_s cached_download/".hg", ".hg"
+      # build.py doesn't run with python3
+      system "python", "build.py", "prepare"
+    end
+
+    ["python2", "python3"].each do |python|
+      version = Language::Python.major_minor_version python
+      system python, "configure.py",
+                     "--deployment-target=#{MacOS.version}",
+                     "--destdir=#{lib}/python#{version}/site-packages",
+                     "--bindir=#{bin}",
+                     "--incdir=#{include}",
+                     "--sipdir=#{HOMEBREW_PREFIX}/share/sip"
+      system "make"
+      system "make", "install"
+      system "make", "clean"
+    end
+  end
+
+  def post_install
+    (HOMEBREW_PREFIX/"share/sip").mkpath
+  end
+
+  def caveats; <<~EOS
+    The sip-dir for Python is #{HOMEBREW_PREFIX}/share/sip.
+  EOS
+  end
+
+  test do
+    (testpath/"test.h").write <<~EOS
+      #pragma once
+      class Test {
+      public:
+        Test();
+        void test();
+      };
+    EOS
+    (testpath/"test.cpp").write <<~EOS
+      #include "test.h"
+      #include <iostream>
+      Test::Test() {}
+      void Test::test()
+      {
+        std::cout << "Hello World!" << std::endl;
+      }
+    EOS
+    (testpath/"test.sip").write <<~EOS
+      %Module test
+      class Test {
+      %TypeHeaderCode
+      #include "test.h"
+      %End
+      public:
+        Test();
+        void test();
+      };
+    EOS
+    (testpath/"generate.py").write <<~EOS
+      from sipconfig import SIPModuleMakefile, Configuration
+      m = SIPModuleMakefile(Configuration(), "test.build")
+      m.extra_libs = ["test"]
+      m.extra_lib_dirs = ["."]
+      m.generate()
+    EOS
+    (testpath/"run.py").write <<~EOS
+      from test import Test
+      t = Test()
+      t.test()
+    EOS
+    system ENV.cxx, "-shared", "-Wl,-install_name,#{testpath}/libtest.dylib",
+                    "-o", "libtest.dylib", "test.cpp"
+    system bin/"sip", "-b", "test.build", "-c", ".", "test.sip"
+
+    ["python2", "python3"].each do |python|
+      version = Language::Python.major_minor_version python
+      ENV["PYTHONPATH"] = lib/"python#{version}/site-packages"
+      system python, "generate.py"
+      system "make", "-j1", "clean", "all"
+      system python, "run.py"
+    end
+  end
+end
